515320dd9e585081e38d7cd54f12067865da2644
Emily <emily.jones@bison.howard.edu>
2024-10-15 10:45:46 -0400

    For LSP Fall 2024 Midterm Exam

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.midterm.question1;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
