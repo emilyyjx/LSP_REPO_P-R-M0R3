f6a3daffc87add1a6e0fae416a0734881f4a4455
Emily <emily.jones@bison.howard.edu>
2024-12-05 21:00:12 -0500

    FINAL EXAM QUESTIONS 1 & 3

--- LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question1/MathUtilsTest.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question1/MathUtilsTest.java
@@ -4,72 +4,65 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class MathUtilsTest {
+    MathUtils mathUtils = new MathUtils();
 
+    // Factorial Tests
     @Test
-    void testFactorial_ValidInput() {
-        MathUtils mathUtils = new MathUtils();
-        assertEquals(120, mathUtils.factorial(5));
-        assertEquals(1, mathUtils.factorial(0));
-    }
-
-    private void assertEquals(int i, int factorial) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Test
-    void testFactorial_NegativeInput() {
-        MathUtils mathUtils = new MathUtils();
-        assertThrows(IllegalArgumentException.class, () -> mathUtils.factorial(-1));
-    }
-
-    private void assertThrows(Class<IllegalArgumentException> class1, Object object) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void assertThrows(Class<IllegalArgumentException> class1, Object object) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Test
-    void testIsPrime_PrimeNumber() {
-        MathUtils mathUtils = new MathUtils();
-        assertTrue(mathUtils.isPrime(7));
-        assertTrue(mathUtils.isPrime(13));
-    }
-
-    private void assertTrue(boolean prime) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Test
-    void testIsPrime_NonPrimeNumber() {
-        MathUtils mathUtils = new MathUtils();
-        assertFalse(mathUtils.isPrime(4));
-        assertFalse(mathUtils.isPrime(1));
-    }
-
-    private void assertFalse(boolean prime) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Test
-    void testGcd_ValidInputs() {
-        MathUtils mathUtils = new MathUtils();
-        assertEquals(6, mathUtils.gcd(54, 24));
-        assertEquals(1, mathUtils.gcd(17, 13));
+    void testFactorialOfZero() {
+        assertEquals(1, mathUtils.factorial(0), "Factorial of 0 should be 1");
     }
 
     @Test
-    void testGcd_ZeroInputs() {
-        MathUtils mathUtils = new MathUtils();
-        assertEquals(10, mathUtils.gcd(0, 10));
-        assertEquals(10, mathUtils.gcd(10, 0));
-        assertThrows(IllegalArgumentException.class, () -> mathUtils.gcd(0, 0));
+    void testFactorialOfPositiveNumber() {
+        assertEquals(120, mathUtils.factorial(5), "Factorial of 5 should be 120");
+    }
+
+    @Test
+    void testFactorialOfNegativeNumberThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.factorial(-1),
+                "Factorial of negative numbers should throw IllegalArgumentException");
+    }
+
+    // Prime Number Tests
+    @Test
+    void testIsPrimeWithPrimeNumber() {
+        assertTrue(mathUtils.isPrime(7), "7 should be prime");
+    }
+
+    @Test
+    void testIsPrimeWithNonPrimeNumber() {
+        assertFalse(mathUtils.isPrime(10), "10 should not be prime");
+    }
+
+    @Test
+    void testIsPrimeWithEdgeCase() {
+        assertFalse(mathUtils.isPrime(1), "1 should not be prime");
+        assertFalse(mathUtils.isPrime(0), "0 should not be prime");
+        assertFalse(mathUtils.isPrime(-5), "-5 should not be prime");
+    }
+
+    // GCD Tests
+    @Test
+    void testGCDOfTwoPositiveNumbers() {
+        assertEquals(6, mathUtils.gcd(54, 24), "GCD of 54 and 24 should be 6");
+    }
+
+    @Test
+    void testGCDWithOneZeroInput() {
+        assertEquals(5, mathUtils.gcd(0, 5), "GCD of 0 and 5 should be 5");
+        assertEquals(7, mathUtils.gcd(7, 0), "GCD of 7 and 0 should be 7");
+    }
+
+    @Test
+    void testGCDWithBothZeroThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> mathUtils.gcd(0, 0),
+                "GCD of 0 and 0 should throw IllegalArgumentException");
     }
 }
